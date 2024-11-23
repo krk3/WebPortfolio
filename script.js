db10729ed8be5c9e95f3be8d57f27f0c62479d01
krk3 <krk253434@gmail.com>
2024-11-23 21:06:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Puedes agregar interactividad en esta sección si lo necesitas, como por ejemplo animaciones o validaciones de formularios
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página cargada correctamente');
+});
